2fc8fadaac3fac75c7fc247e2829b158edd8280b
Yinon <yinon.lehner@gmail.com>
2022-09-03 20:00:15 +0300

    תחילת כתיבת סוקטים צד שרת

--- IOs/SocketIO.cpp
+++ IOs/SocketIO.cpp
@@ -1,9 +1,56 @@
 #include "SocketIO.hpp"
 
-void SocketIO::write(string str) {
+SocketIO() {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("error creating socket");
+    }
+    m_socket = sock;
+}
 
+void SocketIO::write(string str) {
+    int length = str.length();
+    int sent_bytes = send(client_sock, str.c_str(), length, 0);
+    if (sent_bytes < 0) {
+        perror("error sending to client");
+    }
 }
 
 string SocketIO::read() {
+    struct sockaddr_in sin;
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(server_port);
+
+    if (bind(m_socket, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
+        perror("error binding socket");
+    }
     
+    if (listen(m_socket, 5) < 0) {
+        perror("error listening to a socket");
+    }
+
+    struct sockaddr_in client_sin;
+    unsigned int addr_len = sizeof(client_sin);
+    int client_sock = accept(m_socket,  (struct sockaddr *) &client_sin,  &addr_len);
+
+    if (client_sock < 0) {
+        perror("error accepting client");
+    }
+
+    char buffer[4096];
+    int expected_data_len = sizeof(buffer);
+    int read_bytes = recv(client_sock, buffer, expected_data_len, 0);
+    string types = "";
+    if (read_bytes == 0) {
+        close(client_sock);
+        return "";
+    }
+    else if (read_bytes < 0) {
+        perror("error");
+    }
+    else {
+        return read_bytes;
+    }    
 }
--- IOs/SocketIO.hpp
+++ IOs/SocketIO.hpp
@@ -2,10 +2,22 @@
 #define SOCKETIO
 
 #include "DefaultIO.hpp"
+#include <iostream>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include "FilesFunc.hpp"
+#include "Iris.hpp"
 using namespace std;
 
 class SocketIO : public DefaultIO{
+    private:
+        int m_socket;
     public:
+        SocketIO();
         void write(string str) override;
         string read() override;
 
